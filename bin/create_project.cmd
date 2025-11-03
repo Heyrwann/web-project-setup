51682e48fd87550ab2b3866388025bf29ee99ae2
Heyrwann <leguennic.erwann.shp@gmail.com>
2025-11-03 16:26:20 +0100

    feature(script): add project folder creation and UTF-8 support

---
+++ bin/create_project.cmd
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001 > nul
+
+::=========================================
+:: Création de la structure d'un projet Web
+::=========================================
+
+echo [BIP] Début du programme... [BOOP]
+
+::===
+:: Créer la structure du projet web
+::===
+
+:: Création du dossier projet et déplacement dedans
+echo [BIP] Création du dossier projet [BOOP]
+mkdir "example-project"
+cd example-project
