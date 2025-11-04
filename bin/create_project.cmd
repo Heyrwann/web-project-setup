8f1766b03f3e4122132cbd48975f67085c0ed8d0
Heyrwann <leguennic.erwann.shp@gmail.com>
2025-11-04 10:13:36 +0100

    feature(script): implement project structure creation in .cmd script - Add functionality to create a web project structure in the .cmd script, like in the .sh script.

--- bin/create_project.cmd
+++ bin/create_project.cmd
@@ -35,4 +35,102 @@ echo. > .editorconfig
 cd .well-known
 echo. > security.txt
 cd ..
-echo "[BIP] Dossier .well-known/ créé [BOOP]"
+echo [BIP] Dossier .well-known/ créé [BOOP]
+
+:: Remplissage de assets/
+cd assets
+mkdir css fonts js
+cd css
+echo. > style.css
+echo. > style.css.map
+cd ../fonts
+echo. > NotoSans-VariableFont.woff2
+echo. > OFL.txt
+echo. > README.txt
+cd ../js
+echo. > app.js
+cd ../..
+echo [BIP] Dossier assets/ créé [BOOP]
+
+:: Remplissage de img/
+cd img
+mkdir icons
+cd ..
+echo [BIP] Création du dossier img/ [BOOP]
+
+:: Remplissage de pages/
+cd pages
+echo. > about.html
+echo. > article-details.html
+echo. > blog.html
+echo. > contact.html
+echo. > cookies-policy.html
+echo. > faq.html
+echo. > legal-notice.html
+echo. > portfolio.html
+echo. > privacy-policy.html
+echo. > project-details.html
+echo. > search-results.html
+echo. > service-details.html
+echo. > services.html
+echo. > team.html
+echo. > terms-conditions.html
+echo. > testimonials.html
+cd ..
+echo [BIP] Dossier pages/ créé [BOOP]
+
+:: Remplissage de scss/
+cd scss
+mkdir abstracts base components layouts pages utilities
+echo. > style.scss
+cd abstracts
+echo. > _function.scss
+echo. > _index.scss
+echo. > _mixins.scss
+echo. > _variables.scss
+cd ../base
+echo. > _fonts.scss
+echo. > _global.scss
+echo. > _index.scss
+echo. > _reset.scss
+echo. > _root.scss
+echo. > _typography.scss
+cd ../components
+echo. > _buttons.scss
+echo. > _card.scss 
+echo. > _icons.scss 
+echo. > _index.scss
+cd ../layouts
+echo. > _container.scss
+echo. > _footer.scss
+echo. > _header.scss
+echo. > _index.scss
+cd ../pages
+echo. > _about.scss
+echo. > _home.scss
+echo. > _index.scss
+cd ../utilities
+echo. > _accessibility.scss
+echo. > _alignment.scss
+echo. > _display.scss
+echo. > _flexbox.scss
+echo. > _grid.scss
+echo. > _index.scss
+echo. > _spacing.scss
+echo. > _visibility.scss
+cd ../..
+echo [BIP] Dossier scss/ créé [BOOP]
+
+:: Remplissage de docs/
+cd docs
+mkdir architecture diagrams security
+echo. > workflow.md
+echo. > README.md
+cd architecture
+echo. > overview.md
+cd ..
+echo [BIP] Dossier docs/ créé [BOOP]
+cd ..
+
+:: Programme terminé
+echo [BIP] ...Programme terminé [BIP]
